40c86d65e60d151ca547d2c327f3031672d26741
officialkinglot <146046831+officialkinglot@users.noreply.github.com>
2025-03-07 13:46:51 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,28 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        'react-router-dom',
-        'react-toastify',
-        'react-toastify/dist/ReactToastify.css',
-        'axios', // Externalize axios
-        'lodash',
-        'moment',
-        'moment-timezone',
-        'react-icons',
-        'react-icons/ai',
-        'uuid',
-        'firebase',
-        'date-fns',
-      ],
-    },
-  },
   resolve: {
     alias: {
       '@': '/src',
@@ -39,7 +17,7 @@ export default defineConfig({
       'react-dom',
       'react-router-dom',
       'react-toastify',
-      'axios', // Pre-bundle axios
+      'axios',
       'lodash',
       'moment',
       'moment-timezone',
